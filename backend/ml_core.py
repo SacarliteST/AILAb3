dcbc8084c7085973064c1bb6eaec46bec3a3390e
SacarliteST <vladen.bokovoi@mail.ru>
2026-05-20 20:58:37 +0300

    Update ml_core.py

--- backend/ml_core.py
+++ backend/ml_core.py
@@ -2,8 +2,7 @@
 from sentence_transformers import SentenceTransformer
 from typing import List
 
-# bge-m3 выдает векторы размерностью 1024
-embedding_model = SentenceTransformer('BAAI/bge-m3')
+embedding_model = SentenceTransformer('all-MiniLM-L6-v2')
 
 
 def get_embedding(text: str) -> List[float]:
